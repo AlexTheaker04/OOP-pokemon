d9cb4ba086e7de26992391a05e453254c4d004b9
Alex <74675759+AlexTheaker04@users.noreply.github.com>
2022-04-21 14:19:53 -0400

    Update testing123.py

--- testing123.py
+++ testing123.py
@@ -1,5 +1,6 @@
 import random
 
+
 # earth gets 5 bonus to special attack
 # fire gets 5 more to defence
 # water get 5 more to heal
@@ -17,6 +18,7 @@ class Monster:
 
     def s_attack(self, target):
         print("special attack")
+        target.attacked(self.damage + 5)
 
     def heal(self):
         temp = self.health
@@ -38,34 +40,52 @@ class Monster:
 
 
 class earth(Monster):
-    print("earth")
+    def s_attack(self, target):
+        print("special attack")
+        target.attacked(self.damage + 10)
 
 
 class water(Monster):
     print("Water")
 
+    def heal(self):
+        temp = self.health
+        self.health += (temp * .2) + 10
+
 
 class fire(Monster):
-    print("Monster")
+
+    def __init__(self, name, type, health, damage, defence):
+        super().__init__(name, type, health, damage, defence + 5)
 
 
 print("Welcome to the battle!")
 print("*****************************")
 print("""
-In this game, you have to fight against either the computer or another player. You take turns attacking or healing your
-Monster. You can enter either "attack", "heal" or "retreat"
+In this game, you have to fight against either the computer or another player.
+You take turns attacking or healing your Monster. You can enter either "attack",
+ "heal" or "retreat"
 """)
 computer = 0
 monster_classes = ["fire", "water", "earth"]
 choice = input("do you want to play against the computer (Y) or another player (N) ? : ")
 choice = choice.upper()
+health1 = 0
+damage1 = 0
+defence1 = 0
 
-name1 = input("Player 1, what name do you want for your monster? ")
-type1 = input("Player, what type (fire, earth, water:? ")
-health1 = int(input("Player what health:? "))
-damage1 = int(input("how much damage:? "))
-defence1 = int(input("How much defence:? "))
-Monster1 = Monster(name1, type1, health1, damage1, defence1)
+while True:  # validation
+    print("Remember, points cannot be more then 50!")
+    name1 = input("Player 1, what name do you want for your monster? ")
+    type1 = input("Player, what type (fire, earth, water:? ")
+    health1 = int(input("Player what health:? "))
+    damage1 = int(input("how much damage:? "))
+    defence1 = int(input("How much defence:? "))
+
+    if health1 + damage1 + defence1 < 51:
+        Monster1 = fire(name1, type1, health1, damage1, defence1)
+        break
+
 
 if choice == "N":
     name2 = input("Player 2, what name do you want for your monster?")
@@ -78,10 +98,15 @@ else:
     computer = 1
     name2 = "El Computer"
     type2 = random.choice(monster_classes)
-    health2 = random.randint(30, 50)
-    damage2 = random.randint(30, 50)
-    defence2 = random.randint(30, 50)
-    Monster2 = Monster(name2, type2, health2, damage2, defence2)
+    health2 = random.randint(1, 30)
+    damage2 = random.randint(5, 10)
+    defence2 = random.randint(3, 10)
+    if type2 == "fire":
+        Monster2 = fire(name2, type2, health2, damage2, defence2)
+    elif type2 == "water":
+        Monster2 = water(name2, type2, health2, damage2, defence2)
+    else:
+        Monster2 = earth(name2, type2, health2, damage2, defence2)
 heal = 1
 if computer == 1:
     while True:
@@ -112,3 +137,8 @@ if computer == 1:
         elif Monster2.health <= 0:
             print("Player wins ")
             break
+        # AI retreat
+        if Monster2.health <=5 and random.randint(1,10) == 5:
+            Monster2.retreat()
+            print(" Monster 2 has retreated, you win!")
+
